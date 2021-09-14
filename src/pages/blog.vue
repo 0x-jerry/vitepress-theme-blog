3a19904866e048d4c191cdba89062e212925f9e4
Jerry Wang <x.jerry.wang@gmail.com>
2021-09-15 00:04:12 +0800

    perf: hide test articles in prod env

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -5,11 +5,13 @@ const isDev = import.meta.env.DEV
 if (isDev) {
   console.log('site config', blogConfig)
 }
+
+const articles = isDev ? blogConfig.articles : blogConfig.articles.filter((o) => o.visible)
 </script>
 
 <template>
   <ul class="flex flex-wrap">
-    <li v-for="o in blogConfig.articles">
+    <li v-for="o in articles">
       <router-link class="mx-10 my-5" :to="o.routePath">
         {{ o.title }}
       </router-link>
