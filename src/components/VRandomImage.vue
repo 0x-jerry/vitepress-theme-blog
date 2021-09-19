f0561c27c45c6a72cd00f48fef27e0de2af2b500
Jerry Wang <x.jerry.wang@gmail.com>
2021-09-19 13:47:50 +0800

    feat: img loading

---
+++ src/components/VRandomImage.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { computed, ref, watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
+import loadingImg from '~/assets/loading.svg'
+
+const props = defineProps<{
+  id?: string
+}>()
+
+const route = useRoute()
+
+const url = computed(() => {
+  return `https://api.tiny-online.online/img/random?id=${props.id || route.path}`
+})
+
+const isLoading = ref(true)
+
+watchEffect(() => {
+  isLoading.value = true
+
+  const img = new Image()
+  img.onload = () => (isLoading.value = false)
+
+  img.src = url.value
+})
+
+const imgUrl = computed(() => (isLoading.value ? loadingImg : url.value))
+</script>
+
+<template>
+  <img :src="imgUrl" display="inline-block" object="cover" />
+</template>
+
+<style></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,13 +2,7 @@
 
 <template>
   <div w="screen" h="screen" overflow="hidden" class="relative" bg="black">
-    <img
-      src="https://api.tiny-online.online/img/random?id=home"
-      w="100vw"
-      h="100vh"
-      display="block"
-      object="cover"
-    />
+    <v-random-image w="screen" h="screen" />
     <div
       class="absolute top-0 left-0"
       h="screen"
@@ -18,47 +12,65 @@
       justify="center"
       align="items-center"
       bg="black opacity-30"
-      text="white"
+      text="light-900 shadow-sm"
     >
-      <div text="shadow-lg">
-        <h1 text="4xl" p="x-10 y-5" font="mono">0x-Jerry's Blog</h1>
-        <br />
-        <ul
-          w="min-content"
-          m="auto"
-          display="flex"
-          justify="center"
-          align="items-center"
-          text="2xl"
-          p="y-3 x-5"
-          border="rounded-md"
-        >
-          <li class="mx-5">
-            <VLink href="/blog" theme="white">
-              <mdi:book></mdi:book>
-            </VLink>
-          </li>
-          <li class="mx-5">
-            <VLink href="/notes" theme="white">
-              <mdi:notebook></mdi:notebook>
-            </VLink>
-          </li>
-          <li class="mx-5">
-            <VLink href="/timeline" theme="white">
-              <mdi:timeline></mdi:timeline>
-            </VLink>
-          </li>
-          <li class="mx-5">
-            <VLink href="https://github.com/0x-jerry" :icon="false" theme="white">
-              <mdi:github></mdi:github>
-            </VLink>
-          </li>
-          <li class="mx-5">
-            <VLink href="mailto:x.jerry.wang@gmail.com" :icon="false" theme="white">
-              <mdi:email></mdi:email>
-            </VLink>
-          </li>
-        </ul>
+      <div
+        class="relative"
+        bg="white"
+        border="rounded-3xl"
+        p="y-20 x-20"
+        overflow="hidden"
+        shadow="lg"
+      >
+        <v-random-image
+          w="screen"
+          h="screen"
+          class="absolute top-1/2 left-1/2"
+          transform="~ -translate-x-1/2 -translate-y-1/2"
+          filter="~ blur-sm brightness-70"
+          display="block"
+          object="cover"
+        />
+        <div class="relative" z="10">
+          <h1 text="4xl" p="x-10 y-5" font="mono">0x-Jerry's Blog</h1>
+          <br />
+          <ul
+            w="min-content"
+            m="auto"
+            display="flex"
+            justify="center"
+            align="items-center"
+            text="2xl"
+            p="y-3 x-5"
+            border="rounded-md"
+          >
+            <li class="mx-5">
+              <VLink href="/blog" theme="white">
+                <mdi:book></mdi:book>
+              </VLink>
+            </li>
+            <li class="mx-5">
+              <VLink href="/notes" theme="white">
+                <mdi:notebook></mdi:notebook>
+              </VLink>
+            </li>
+            <li class="mx-5">
+              <VLink href="/timeline" theme="white">
+                <mdi:timeline></mdi:timeline>
+              </VLink>
+            </li>
+            <li class="mx-5">
+              <VLink href="https://github.com/0x-jerry" :icon="false" theme="white">
+                <mdi:github></mdi:github>
+              </VLink>
+            </li>
+            <li class="mx-5">
+              <VLink href="mailto:x.jerry.wang@gmail.com" :icon="false" theme="white">
+                <mdi:email></mdi:email>
+              </VLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
