af452c671b7656bef424b7d366022274630ea2a0
Jerry Wang <x.jerry.wang@gmail.com>
2021-10-07 15:51:39 +0800

    fix: miss the "from" data

--- src/components/VSentence.vue
+++ src/components/VSentence.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 import { useFetch } from '@vueuse/core'
+import { computed } from 'vue'
 
 const url = 'https://api.0x-jerry.icu/hitokoto?t=' + new Date().getTime()
 
 const { data, isFetching } = useFetch(url).get().json()
+
+const from = computed(() => [data.value?.from, data.value?.from_who].filter(Boolean).join(' - '))
 </script>
 
 <template>
@@ -13,10 +16,7 @@ const { data, isFetching } = useFetch(url).get().json()
   <div v-else class="v-sentence" text="center">
     <p font="italic">「 {{ data?.hitokoto }} 」</p>
     <br />
-    <p text="right">
-      --- 《{{ data?.from }}<span v-if="data?.from_who"> - {{ data?.from_who }}</span
-      >》
-    </p>
+    <p text="right">--- 《{{ from }}》</p>
   </div>
 </template>
 
