74556e46127ce55f260a4d86296d8fb3c9f74d14
0x-jerry <x.jerry.wang@gmail.com>
2021-11-29 14:46:25 +0800

    feat: update sentence when click

--- src/components/VSentence.vue
+++ src/components/VSentence.vue
@@ -1,19 +1,27 @@
 <script lang="ts" setup>
-import { useFetch } from '@vueuse/core'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
-const url = 'https://api.0x-jerry.icu/hitokoto?t=' + new Date().getTime()
+const isFetched = ref(false)
 
-const { data, isFetching } = useFetch(url).get().json()
+const data = ref<any>({})
+
+async function fetchHitokoto() {
+  const url = 'https://api.0x-jerry.icu/hitokoto?t=' + new Date().getTime()
+  const res = await fetch(url)
+  data.value = await res.json()
+  isFetched.value = true
+}
 
 const from = computed(() => [data.value?.from, data.value?.from_who].filter(Boolean).join(' - '))
+
+fetchHitokoto()
 </script>
 
 <template>
-  <template v-if="isFetching">
+  <template v-if="!isFetched">
     <slot></slot>
   </template>
-  <div v-else class="v-sentence" text="center">
+  <div v-else class="v-sentence" text="center" @click="fetchHitokoto" cursor="pointer">
     <p font="italic">「 {{ data?.hitokoto }} 」</p>
     <br />
     <p text="right">--- 《{{ from }}》</p>
