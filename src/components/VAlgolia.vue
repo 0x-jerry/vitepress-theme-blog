0e3e31b363c3f1e8298fd01d8e2b35443629e3f9
Jerry Wang <x.jerry.wang@gmail.com>
2022-07-01 23:49:35 +0800

    fix: appId

--- src/components/VAlgolia.vue
+++ src/components/VAlgolia.vue
@@ -9,7 +9,7 @@ import '@docsearch/css'
 onMounted(() => {
   docsearch({
     container: '#docsearch',
-    appId: 'YOUR_APP_ID',
+    appId: 'XP2151RITM',
     indexName: 'netlify_a59f0d12-ac84-4bf4-a017-7d6fc5be71de_main_all',
     apiKey: '43dc8c4281920640734b034804f37605',
   })
--- src/components/VHeader.vue
+++ src/components/VHeader.vue
@@ -94,7 +94,9 @@ const activeClass = computed(() => {
         <span m="l-2" class="hidden lg:block"> {{ $t('name') }}'s Blog </span>
       </router-link>
     </div>
-    <v-algolia></v-algolia>
+
+    <!-- <v-algolia></v-algolia> -->
+
     <div flex="~ 1" align="items-center" justify="end">
       <v-link
         theme="text"
