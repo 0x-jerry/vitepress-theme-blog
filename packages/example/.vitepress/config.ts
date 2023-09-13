0cd9481df61e86f8f44799f22d292d69431cb8fc
Jerry Wang <x.jerry.wang@gmail.com>
2023-09-13 10:46:24 +0800

    chore: fix build config

--- packages/example/.vitepress/config.ts
+++ packages/example/.vitepress/config.ts
@@ -5,6 +5,8 @@ import { defineConfigWithTheme } from 'vitepress'
 export default async () => {
   return defineConfigWithTheme<ThemeOption>({
     extends: await createBaseConfig(),
+    cleanUrls: true,
+    ignoreDeadLinks: true,
     themeConfig: {
       name: '0x-Jerry',
     },
