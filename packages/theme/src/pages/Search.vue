8e8ec00b02cf0cc4749b17bb14b7f066b5d79342
Jerry <x.jerry.wang@gmail.com>
2023-10-07 16:00:49 +0800

    perf: improve search page

--- packages/theme/src/pages/Search.vue
+++ packages/theme/src/pages/Search.vue
@@ -1,32 +1,45 @@
 <script setup>
 import VTitle from '@@/components/VTitle.vue'
 import { useI18n } from '@@/lib/i18n'
-import { reactive, watch } from 'vue'
+import { useRouter } from 'vitepress'
+import { reactive, shallowRef, watch } from 'vue'
 
 const { t } = useI18n()
 
 const state = reactive({
-  input: '',
+  input: getSearchFromQuery(),
   results: [],
 })
 
-let pagefind
+const pagefind = shallowRef()
+
+// workaround for loading pagefind js
+const getPageFindJs = Function(`return import("/pagefind/pagefind.js")`)
 
 init()
 
 async function init() {
-  const url = `/pagefind/pagefind.js`
-
-  pagefind = await import(/* @vite-ignore */ url)
-  pagefind.init()
+  pagefind.value = await getPageFindJs()
+  pagefind.value.init()
 }
 
-watch(
-  () => state.input,
-  async () => {
-    if (!pagefind) return
+function getSearchFromQuery() {
+  if (import.meta.env.SSR) return ''
+  const u = new URL(location.href)
 
-    const resp = await pagefind.search(state.input)
+  return u.searchParams.get('s') || ''
+}
+
+const router = useRouter()
+
+watch(
+  () => [state.input, pagefind.value],
+  async () => {
+    if (!pagefind.value) return
+
+    router.go(`/search?s=${state.input}`)
+
+    const resp = await pagefind.value.search(state.input)
 
     const p = resp.results.map((n) => n.data())
 
@@ -37,21 +50,33 @@ watch(
 
 <template>
   <div>
-    <VTitle show-back :title="t('title.search')"></VTitle>
-    <div>
-      <input v-model="state.input" />
+    <VTitle class="mb-3" show-back :title="t('title.search')"></VTitle>
+    <div class="mb-3">
+      <input class="search-input" v-model="state.input" placeholder="Give me something to search" />
     </div>
-    <div>
+    <div class="flex-(~ col) gap-3">
       <div v-for="result in state.results">
-        <div class="title">
+        <div class="title mb-2">
           <a :href="result.url">
             {{ result.meta.title }}
           </a>
         </div>
-        <div v-html="result.excerpt"></div>
+        <div class="p-2 border-(1 solid gray-2) heti max-w-full">
+          <div v-html="result.excerpt"></div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.search-input {
+  @apply w-full border-(1 solid gray-3) rounded;
+  @apply text-lg;
+
+  &:focus {
+    @apply border-(blue-4);
+    outline: none;
+  }
+}
+</style>
