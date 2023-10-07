5bded1e603c538abf81fc36acd3190cb5cdc72b3
Jerry <x.jerry.wang@gmail.com>
2023-10-07 11:39:19 +0800

    perf: tweak sidebar style

--- packages/theme/src/components/VButton.vue
+++ packages/theme/src/components/VButton.vue
@@ -2,7 +2,7 @@
 
 <template>
   <button
-    class="px-2 py-1 border-(gray-100 1 solid) cursor-pointer bg-gray-1 hover:bg-gray-2 inline-flex items-center justify-center"
+    class="p-2 border-(gray-100 1 solid) cursor-pointer bg-gray-1 hover:bg-gray-2 inline-flex items-center justify-center"
   >
     <slot></slot>
   </button>
