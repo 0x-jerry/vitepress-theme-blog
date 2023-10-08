a6e5a980fbea40709c77186c9528dfde1489a1fe
Jerry <x.jerry.wang@gmail.com>
2023-10-08 12:18:36 +0800

    perf: update footer

--- .theme/src/components/VFooter.vue
+++ .theme/src/components/VFooter.vue
@@ -1,15 +1,17 @@
 <script lang="ts" setup>
 import dayjs from 'dayjs'
 import VLink from './VLink.vue'
+import { useTheme } from '@@/hooks/useTheme'
 
 const year = dayjs.tz().year()
+
+const name = useTheme().value.name
 </script>
 
 <template>
   <div class="w-full text-center mt-4 my-6">
-    © 2017-{{ year }} @ 0x-Jerry. Built top on
-    <v-link href="https://vitejs.dev/"> vite </v-link>
-    and
-    <v-link href="https://v3.vuejs.org/"> vue </v-link>.
+    © 2017-{{ year }} @ {{ name }}. Built top on
+    <v-link href="https://vitepress.dev/"> Vitepress </v-link>
+    .
   </div>
 </template>
