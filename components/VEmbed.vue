c422ef7cc83ae737599668240e15513f02148388
Jerry <x.jerry.wang@gmail.com>
2024-06-01 11:15:40 +0800

    chore: test embed component

---
+++ components/VEmbed.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { CSSProperties, computed } from 'vue'
+
+export type IframeData = string
+
+const props = withDefaults(
+  defineProps<{
+    type?: 'iframe'
+    data: IframeData
+    width?: number | string
+    height?: number | string
+    aspect?: string
+  }>(),
+  {
+    type: 'iframe',
+    width: '100%',
+    aspect: '16/9'
+  }
+)
+
+const style = computed(() => {
+  const s: CSSProperties = {
+    aspectRatio: props.aspect,
+    width: props.width,
+    height: props.height
+  }
+
+  return s
+})
+</script>
+
+<template>
+  <iframe :src="data" :style="style" frameborder="0"></iframe>
+</template>
+
+<style lang="less" scoped></style>
