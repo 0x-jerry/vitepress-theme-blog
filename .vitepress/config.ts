44f208c3234af9e38622ed753bc93ab9d81e8748
Jerry <x.jerry.wang@gmail.com>
2024-06-04 23:37:13 +0800

    chore: remove pwa

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,45 +1,9 @@
 import type { ThemeOption } from '@0x-jerry/vitepress-theme-blog'
 import createBaseConfig from '../.theme/vitepress/config'
 import { defineConfigWithTheme } from 'vitepress'
-import { withPwa } from '@vite-pwa/vitepress'
 
 export default async () => {
   const conf = defineConfigWithTheme<ThemeOption>({
-    pwa: {
-      manifest: {
-        name: '0x-Jerry | Blog',
-        description: "0x-Jerry's Blog",
-        icons: [
-          {
-            src: 'pwa-192x192.webp',
-            sizes: '192x192'
-          },
-          {
-            src: 'favicon.svg',
-            sizes: '512x512',
-            purpose: 'maskable'
-          },
-          {
-            src: 'favicon.svg',
-            sizes: '512x512',
-            purpose: 'any'
-          }
-        ],
-        theme_color: '#ffffff',
-        screenshots: [
-          {
-            src: 'pwa-192x192.webp',
-            sizes: '192x192',
-            form_factor: 'wide'
-          },
-          {
-            src: 'pwa-192x192.webp',
-            sizes: '192x192',
-            form_factor: 'narrow'
-          }
-        ]
-      }
-    },
     extends: await createBaseConfig(),
     title: '0x-Jerry',
     titleTemplate: ':title | Blog',
@@ -57,5 +21,5 @@ export default async () => {
     }
   })
 
-  return withPwa(conf)
+  return conf
 }
